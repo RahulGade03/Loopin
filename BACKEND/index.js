f8ed872c158559f67146805041a201e0502c7f7f
RahulGade03 <rahulgade2004@gmail.com>
2025-07-10 00:38:23 +0530

    ready for deployment

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,4 +1,4 @@
-import express, {urlencoded} from 'express';
+import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 // console.log (__dirname);
 
-app.get ("/", ( req, res) => {
+app.get ("/api", ( req, res) => {
     return res.status(200).json ({
         message: "Welcome to the backend server!",
         success: true
@@ -27,7 +27,7 @@ app.get ("/", ( req, res) => {
 //Middlewares
 app.use (express.json());
 app.use (cookieParser());
-app.use (urlencoded({ extended: true, limit:'100mb' }));
+app.use (express.urlencoded({ extended: true, limit:'100mb' }));
 app.use (cors ({
     origin: "http://localhost:5173",
     credentials: true,
@@ -37,9 +37,9 @@ app.use ("/api/v1/message", messageRoute);
 app.use ("/api/v1/post", postRoute);
 
 app.use (express.static(path.join(__dirname, "/FRONTEND/dist")));
-// app.get("*", (req, res) => {
-//     res.sendFile (path.resolve(__dirname, "FRONTEND", "dist", "index.html"));
-// })
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile (path.resolve(__dirname, "FRONTEND", "dist", "index.html"));
+})
 
 server.listen (PORT, async () => {
     await connectDB ();
