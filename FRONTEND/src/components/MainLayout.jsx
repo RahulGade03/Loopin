d8d23883056bf49fb1c29720441c5281903976e2
RahulGade03 <rahulgade2004@gmail.com>
2026-04-04 23:18:43 +0530

    ...

--- FRONTEND/src/components/MainLayout.jsx
+++ FRONTEND/src/components/MainLayout.jsx
@@ -5,7 +5,7 @@ const MainLayout = () => {
   return (
     <div className="flex max-w-[1200px] w-full mx-auto">
         <LeftSideBar/>
-        <div className="ml-[15vw]">
+        <div className="ml-[13vw]">
           <Outlet/>
         </div>
     </div>
