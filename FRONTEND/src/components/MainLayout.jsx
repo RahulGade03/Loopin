b2240a2f031945689fa6a746464963f1ed49ecf8
RahulGade03 <rahulgade2004@gmail.com>
2026-04-04 22:40:52 +0530

    trying to fix zoom issue on vercel

--- FRONTEND/src/components/MainLayout.jsx
+++ FRONTEND/src/components/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import LeftSideBar from "./LeftSideBar.jsx";
 const MainLayout = () => {
   return (
-    <div className="flex">
+    <div className="flex max-w-[1200px] w-full mx-auto">
         <LeftSideBar/>
         <Outlet/>
     </div>
